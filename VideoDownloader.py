df340766161d8bad2f7d7b15ef35ba805cfbaf59
Igor Picoli <igorepicoli@gmail.com>
2020-04-27 21:52:08 -0300

    Downloader Adicionado

---
+++ VideoDownloader.py
@@ -0,0 +1,51 @@
+import os
+from pytube import YouTube
+
+def Download():
+    print('\n')
+    #YouTube('https://youtu.be/9bZkp7q19f0').streams.first().download()
+
+    #link do video
+
+    link = input('Insira a url do vídeo: ')
+    link = YouTube(link)
+
+    print('===================================================')
+    print('\n\n\n\n\n')
+    print(link.title)
+
+    stream = link.streams.first()
+
+    stream.download('')
+    print('\n\n\n\n\n')
+
+    print('> Download realizado com sucesso! < ')
+
+
+
+def Intro():
+
+    print('    ____                    ____  _            ___')
+    print('   /  _/___ _____  _____   / __ \(_)________  / (_)')
+    print('   / // __ `/ __ \/ ___/  / /_/ / / ___/ __ \/ / /')
+    print(' _/ // /_/ / /_/ / /     / ____/ / /__/ /_/ / / /')
+    print('/___/\__, /\____/_/     /_/   /_/\___/\____/_/_/')
+    print('    /____/')
+
+    print('        __   __          _         _          ')
+    print('        \ \ / /         | |       | |         ')
+    print('         \ V /___  _   _| |_ _   _| |__   ___ ')
+    print('          \ // _ \| | | | __| | | | ''_  \ / _ \'')
+    print('          | | (_) | |_| | |_| |_| | |_) |  __/')
+    print('          \_/\___/ \__,_|\__|\__,_|_.__/ \___|')
+    print('')
+
+    print('    ____                      __                __')
+    print('   / __ \____ _      ______  / /___  ____ _____/ /__  _____')
+    print('  / / / / __ \ | /| / / __ \/ / __ \/ __ `/ __  / _ \/ ___/')
+    print(' / /_/ / /_/ / |/ |/ / / / / / /_/ / /_/ / /_/ /  __/ /')
+    print('/_____/\____/|__/|__/_/ /_/_/\____/\__,_/\__,_/\___/_/')
+    print('')
+
+Intro()
+Download()
